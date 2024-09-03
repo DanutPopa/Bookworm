cfbb30ea86617c99e0a73fc28ad2f9b493254ba0
DanutPopa <popa.ion.danut@gmail.com>
2024-09-03 13:03:13 +0300

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Danut Popa on 03.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
