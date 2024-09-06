d9e2b0e99768e5f463060950407659ba66beddd0
DanutPopa <popa.ion.danut@gmail.com>
2024-09-06 20:25:52 +0300

    Adding a book and save it using the model context.

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -5,19 +5,30 @@
 //  Created by Danut Popa on 03.09.2024.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var books: [Book]
+    
+    @State private var showingAddScreen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            
-            Text("Hello, world!")
-                .font(.subheadline)
+        NavigationStack {
+            Text("Count: \(books.count)")
+                .navigationTitle("Bookwhorm")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("Add Book", systemImage: "plus") {
+                            showingAddScreen.toggle()
+                        }
+                    }
+                }
+                .sheet(isPresented: $showingAddScreen) {
+                    AddBookView()
+                }
         }
-        .padding()
     }
 }
 
