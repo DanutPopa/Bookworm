d9c906c8cc9ae93bc31651ca1482920b84f45994
DanutPopa <popa.ion.danut@gmail.com>
2024-09-07 08:33:39 +0300

    Creating a custom star component to rate a book.

---
+++ Bookworm/RatingView.swift
@@ -0,0 +1,18 @@
+//
+//  RatingView.swift
+//  Bookworm
+//
+//  Created by Danut Popa on 06.09.2024.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingView()
+}
